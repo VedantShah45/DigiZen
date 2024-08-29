a988b5603a02f4714b2e90a7f5f91fc7cc991d21
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-08-29 23:11:22 +0530

    created home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,40 +1,23 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
-import useShowToast from '../hooks/useShowToast';
-import axios from 'axios';
-import BASE_URL from '../utils/baseurl.js';
-import { setUser } from '../redux/authSlice';
 import Navbar from '../components/Navbar.jsx';
+import Hero from '../components/Hero.jsx';
+import Description from '../components/Description.jsx';
+import Footer from '../components/Footer.jsx';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-    const navigate = useNavigate();
     const { user } = useSelector(store => store.auth);
-    const showToast = useShowToast();
-    const dispatch = useDispatch();
+    const navigate = useNavigate();
     useEffect(() => {
         if (!user) navigate('/login');
     }, []);
-    const handleLogout = async () => {
-        try {
-            const response = await axios.get(`${BASE_URL}/user/logout`, { withCredentials: true });
-            if (response.data.success) {
-                dispatch(setUser(null));
-                showToast("Logged out", response.data.message, "success");
-                navigate('/login');
-            }
-        } catch (error) {
-            console.log(error);
-            showToast("Error", error.response.data.message, "error");
-        }
-    }
     return (
         <>
             <Navbar />
-            <div className='min-h-screen bg-teal-300 flex flex-col items-center justify-center'>
-                WELCOME TO HOME PAGE!
-                <button onClick={handleLogout} className='bg-teal-500 hover:bg-teal-700 text-white p-2 rounded-lg mt-2 font-bold'>LOGOUT!</button>
-            </div>
+            <Hero />
+            <Description />
+            <Footer />
         </>
     )
 }
